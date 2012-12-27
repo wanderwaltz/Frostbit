5eb6a3f3267336e4e2caf88a8387db4a1517c8ed
wanderwaltz <wanderwaltz@gmail.com>
2012-12-28 01:51:36 +0700

    More assertion #defines. Comments for assertion #defines.

--- Frostbit/Common/Meta/FRBAssertMacros.h
+++ Frostbit/Common/Meta/FRBAssertMacros.h
@@ -9,39 +9,63 @@
 #ifndef Frostbit_FRBAssertMacros_h
 #define Frostbit_FRBAssertMacros_h
 
-/*! Macros defined in this file are used to shorten assertions with
-    possibly complex and informative messages.
+/* Macros defined in this file are used to shorten assertions with possibly complex and informative messages.
  */
 
+
+/* Asserts that the Object is kind of the Class provided. To shorten the statement, class name should be passed to the macro directly, for example FRB_AssertClass(@"Hello, World!", NSString);
+ 
+ Note that this will fail for nil objects.
+ */
 #define FRB_AssertClass(Object, Class) \
     NSAssert([Object isKindOfClass: [Class class]], \
              @"Expected " @#Object " to be of class " @#Class @", got %@ instead", \
              [Object class])
 
 
+/* A variation of FRB_AssertClass, but will not fail for nil objects.
+ */
+#define FRB_AssertClassOrNil(Object, Class) \
+    NSAssert(((Object == nil) || [Object isKindOfClass: [Class class]]), \
+             @"Expected " @#Object " to be of class " @#Class @", got %@ instead", \
+            [Object class])
+
+
+
+/* Asserts that the Class is a subclass of Superclass provided. To shorten the statement, class names should be passed to the macro directly, for example FRB_AssertIsSubclassOfClass(UILabel, UIView);
+ */
 #define FRB_AssertIsSubclassOfClass(Class, Superclass) \
     NSAssert([Class isSubclassOfClass: [Superclass class]], \
              @"Expected " @#Class " to be a subclass of " @#Superclass @", got %@ instead", \
              [Class class])
 
 
-#define FRB_AssertClassOrNil(Object, Class) \
-    NSAssert(((Object == nil) || [Object isKindOfClass: [Class class]]), \
-             @"Expected " @#Object " to be of class " @#Class @", got %@ instead", \
-             [Object class])
 
-
+/* Asserts that the Object responds to the Selector provided. Selector should be passed as SEL or @selector, for example FRB_AssertResponds(@"Hello, World!", @selector(length));
+ */
 #define FRB_AssertResponds(Object, Selector) \
     NSAssert([Object respondsToSelector: Selector], \
              @"Expected " @#Object " (%@) to respond to %@", \
              Object, NSStringFromSelector(Selector))
 
 
+/* Always failing assertion with a self-describing name.
+ */
 #define FRB_ShouldNeverHappen(Description, ...) \
     NSAssert(NO, Description, ##__VA_ARGS__)
 
 
+
+/* Asserts that the Object is not nil.
+ */
 #define FRB_AssertNotNil(Object) \
     NSAssert(Object != nil, @"")
+
+
+/* Asserts that Object conforms to the Protocol provided.
+ */
+#define FRB_AssertConformsTo(Object, Protocol) \
+    NSAssert([Object conformsToProtocol: @protocol(Protocol)],\
+             @"Expected " @#Object " (%@) to conform to protocol " @#Protocol)
 
 #endif
